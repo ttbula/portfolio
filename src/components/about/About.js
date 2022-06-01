b9eb886aea785263957a36ba93ed429a353754d5
Timothy Cebula <timothycebula@gmail.com>
2022-06-01 15:43:49 -0400

    about.me info added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,6 +5,23 @@ const About = () => {
         <div className="about">
             <h5>Get to know Tim</h5>
             <h2>About Me</h2>
+            <div className='about_content'>
+                <div className='about_cards'>
+                     <article className='about_card'>
+                         <h5>Experience</h5>
+                         <small>5+ months of software development</small>
+                     </article>
+                     <article className='about_card'>
+                         <h5>Military</h5>
+                         <small>Air Force veteran with 7 years of active duty service</small>
+                     </article>
+                     <article className='about_card'>
+                         <h5>Projects</h5>
+                         <small>3+ completed projects</small>
+                     </article>
+                </div>
+                <p>An Air Force veteran turned software developer who enjoys tackling new tasks and gaining skills and is a lifelong learner.</p>
+            </div>
         </div>
     )
 }
